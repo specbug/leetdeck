966022eb5d4c2a7053d623ebc1e567ae8f470084
specbug <rishit7.rv@gmail.com>
2023-02-12 03:55:43 +0530

    move parsing logic to popup.js

--- popup.js
+++ popup.js
@@ -1,11 +1,35 @@
-// popup.js
+const observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+    const buttons = document.querySelectorAll('button');
+    if (buttons.length) {
+      observer.disconnect();
+      buttons.forEach(function(button) {
+        if (button.innerText == 'Submit') {
+            console.log('adding event to button', button.innerText);
+            button.addEventListener('click', function() {
+              console.log('[leak] Submit button clicked!');
+              var front = '';
+              var back = '<code>';
+              // Code to create flashcard in Anki
+              console.log("[leak] flashcard creation triggered");
+              const paragraphs = document.querySelector('._1l1MA').childNodes;
+              for (const p of paragraphs) {
+                  front += p.textContent + "\n";
+              };
+              console.log('[leak] front\n', front);
+              back += document.querySelector('.monaco-scrollable-element').innerText;
+              back += '</code>';
+              console.log('[leak] back\n', back);
+              // send a message to the background script
+              chrome.runtime.sendMessage({ type: 'submit-button-clicked' , payload: {"front": front, "back": back}});
+        });
+        }
+      });
+    }
+  });
+});
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('loaded DOM content')
-    const submitButton = document.querySelector('button');
-    submitButton.addEventListener('click', function() {
-      console.log('[leak] Submit button clicked!')
-      // send a message to the background script
-      chrome.runtime.sendMessage({ type: 'submit-button-clicked' });
-    });
+observer.observe(document.body, {
+  childList: true,
+  subtree: true
 });
