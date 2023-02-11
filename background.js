95c1bfcccbdfa2cdbcf9a172c0bd7b88d987dec7
specbug <rishit7.rv@gmail.com>
2023-02-12 02:03:45 +0530

    add event listener to background.js

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
-var invoke = function(action, version, params={}) {
+function invoke(action, version, params={}) {
     return new Promise((resolve, reject) => {
         const xhr = new XMLHttpRequest();
         xhr.addEventListener('error', () => reject('failed to issue request'));
@@ -23,54 +23,58 @@ var invoke = function(action, version, params={}) {
             }
         });
 
-        xhr.open('POST', 'http://127.0.0.1:8765');
+        xhr.open('POST', 'http://localhost:8765');
         xhr.send(JSON.stringify({action, version, params}));
     });
 };
 
-var addToAnki = function() {
+function addToAnki() {
   const leetcodeProblemPageRegex = /^https:\/\/leetcode\.com\/problems\/.+/;
   if (leetcodeProblemPageRegex.test(window.location.href)) {
       console.log("Detected LC problem page. `addToAnki` registered.");
-      document.querySelector('button').addEventListener('click', function() {
-        console.log("Submit button clicked");
-      
       const originalFetch = window.fetch;
       window.fetch = function(url, init) {
           console.log("API hit", url);
           if (url.endsWith('/submit/')) {
             // Show confirmation popup
-            if (confirm("Do you want to create a flashcard in Anki?")) {
-              // Code to create flashcard in Anki
-              console.log("Flashcard creation triggered");
-              var front = '';
-              var back = '';
-              const paragraphs = document.querySelector('._1l1MA').childNodes;
-              for (const p of paragraphs) {
-                  front += p.textContent + "\n";
-              };
-              console.log('Copied front\n', front);
-              if (init && init.body) {
-                let payload = JSON.parse(init.body);
-                console.log("Request Payload:", payload);
-                back += payload.typed_code;
-                console.log("Back:\n", back);
-              };
-              invoke('addNote', 6, {
-                "note": {
-                  "deckName": "One Deck To Rule Them All",
-                  "modelName": "Basic",
-                  "fields": {
-                    "Front": front,
-                    "Back": back
-                  },
-                  "tags": []
-                }
-              });
-            }
+            // Code to create flashcard in Anki
+            console.log("Flashcard creation triggered");
+            var front = '';
+            var back = '';
+            const paragraphs = document.querySelector('._1l1MA').childNodes;
+            for (const p of paragraphs) {
+                front += p.textContent + "\n";
+            };
+            console.log('Copied front\n', front);
+            if (init && init.body) {
+              let payload = JSON.parse(init.body);
+              console.log("Request Payload:", payload);
+              back += payload.typed_code;
+              console.log("Back:\n", back);
+            };
+            invoke('addNote', 6, {
+              "note": {
+                "deckName": "One Deck To Rule Them All",
+                "modelName": "Basic",
+                "fields": {
+                  "Front": front,
+                  "Back": back
+                },
+                "tags": []
+              }
+            });
           }
         return originalFetch.apply(this, arguments);
       };
-    });
   };
 };
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+   if (request.type === 'submit-button-clicked') {
+       console.log('[leak] submit button event detected.')
+       addToAnki();
+   }
+   else {
+       console.log('[leak] non-supported event received.', request.type)
+   }
+});
